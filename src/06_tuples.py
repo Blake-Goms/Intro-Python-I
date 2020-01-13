71f66334ea5386ccf018a1c0f173bc9b185a6bbd
Blake Goms <blakegoms@gmail.com>
2020-01-13 13:44:59 -0800

    added comment

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -48,5 +48,6 @@ print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
 u = (1,)  # What needs to be added to make this work?
-# The comma defines the tuple, not the paranthesis because it is a one-element tuple
+# The comma defines the tuple, not the paranthesis because it is a one-element tuple,
+# helps differentiate between a tuple and expression
 print_tuple(u)
